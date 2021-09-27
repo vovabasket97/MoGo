093cb962863dd88a9198181b708207bfc00c00fd
Volodymyr <vovabasket97@gmail.com>
2021-09-27 23:56:09 +0200

    changing

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('loaded');
+    document.querySelectorAll('[href="#"]').forEach(item => item.addEventListener('click', (e) => e.preventDefault()));
     $('.owl-carousel').owlCarousel({
-        items:1,
-        loop:false,
-        center:false,
-        nav:false,
-        dots:false,
-        margin:0,
+        items: 1,
+        loop: false,
+        center: false,
+        nav: false,
+        dots: false,
+        margin: 0,
         loop: true,
-        URLhashListener:true,
-        autoplayHoverPause:true,
+        URLhashListener: true,
+        autoplayHoverPause: true,
         startPosition: 'URLHash',
         mouseDrag: false,
         touchDrag: false
@@ -23,7 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
             target.classList.add('active');
         }
     });
-    jQuery(document).ready(function(){
+    document.querySelector('.nav-close').addEventListener('click', (e) => {
+        e.target.parentNode.classList.remove('active');
+    })
+    document.querySelector('.nav-open').addEventListener('click', (e) => {
+        console.log(e.target);
+        e.target.previousElementSibling.classList.add('active');
+    })
+    jQuery(document).ready(function () {
         jQuery('.what-we-do .accordion-body').scrollbar();
     });
 });
